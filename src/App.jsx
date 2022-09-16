4bca343a8ac7c5ec7b8df2ba00d3e78b92b3ac06
muftifachrudin <muftiarifachrudin@gmail.com>
2022-09-16 16:46:10 +0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Admin from './pages/Admin';
+import Cart from './pages/Cart';
+import History from './pages/History';
+import ProductDetail from './pages/ProductDetail'
 
 const App = () => {
   return (
-    <div>
-      <h1>React JS Template</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/ProductDetail" element={<ProductDetail />} />
+      </Routes>
+    </Router>
   )
 }
-
 export default App;
