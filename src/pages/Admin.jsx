dbdd655990595bc12af83bec6c04daf386f22e72
muftifachrudin <muftiarifachrudin@gmail.com>
2022-09-22 02:50:31 +0700

    create data

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,81 @@
 import React from "react";
+import Axios from "axios";
+import { API_URL } from "../constants/API";
+import "../assets/styles/admin.css";
 
 class Admin extends React.Component {
+  state = {
+    productList: [],
+    addProductName: "",
+    addProductPrice: "",
+    addProductImage: "",
+    addDescription: "",
+    addCategory: "",
+  };
+
+  fetchProducts = () => {
+    Axios.get(`${API_URL}/products`)
+      .then((result) => {
+        this.setState({ productList: result.data });
+      })
+      .catch(() => {
+        alert("Terjadi kesalahan diserver");
+      });
+  };
+
+  renderProducts = () => {
+    return this.state.productList.map((val) => {
+      return (
+        <tr>
+          <td>{val.id}</td>
+          <td>{val.productName}</td>
+          <td>{val.price}</td>
+          <td>
+            <img
+              className="admin-product-image"
+              src={val.productImage}
+              alt=""
+            />
+          </td>
+          <td>{val.description}</td>
+          <td>{val.category}</td>
+          <td>
+            <button className="btn btn-secondary">Edit</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  addNewProduct = () => {
+    Axios.post(`${API_URL}/products`, {
+      productName: this.state.addProductName,
+      price: this.state.addProductPrice,
+      productImage: this.state.addProductImage,
+      description: this.state.addDescription,
+      category: this.state.addCategory,
+    })
+      .then(() => {
+        this.fetchProducts();
+      })
+      .catch(() => {
+        alert("Terjadi Kesalahan add Product!");
+      });
+  };
+
+  inputHandler = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
   render() {
     return (
       <div className="p-5">
@@ -19,47 +94,61 @@ class Admin extends React.Component {
                   <th colSpan={2}>Action</th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <button className="btn btn-secondary">Edit</button>
-                  </td>
-                  <td>
-                    <button className="btn btn-danger">Delete</button>
-                  </td>
-                </tr>
-              </tbody>
+              <tbody>{this.renderProducts()}</tbody>
               <tfoot className="bg-light">
                 <tr>
                   <td></td>
                   <td>
-                    <input type="text" className="form-control" />
+                    <input
+                      onChange={this.inputHandler}
+                      name="addProductName"
+                      type="text"
+                      className="form-control"
+                    />
                   </td>
                   <td>
-                    <input type="text" className="form-control" />
+                    <input
+                      onChange={this.inputHandler}
+                      name="addProductPrice"
+                      type="text"
+                      className="form-control"
+                    />
                   </td>
                   <td>
-                    <input type="text" className="form-control" />
+                    <input
+                      onChange={this.inputHandler}
+                      name="addProductImage"
+                      type="text"
+                      className="form-control"
+                    />
                   </td>
                   <td>
-                    <input type="text" className="form-control" />
+                    <input
+                      onChange={this.inputHandler}
+                      name="addDescription"
+                      type="text"
+                      className="form-control"
+                    />
                   </td>
                   <td>
-                    <select name="addCategory" className="form-control">
+                    <select
+                      onChange={this.inputHandler}
+                      name="addCategory"
+                      className="form-control"
+                    >
                       <option value="">All Items</option>
-                      <option value="baju">Baju</option>
-                      <option value="topi">Topi</option>
-                      <option value="celana">Celana</option>
+                      <option value="Baju">Baju</option>
+                      <option value="Topi">Topi</option>
+                      <option value="Celana">Celana</option>
                     </select>
                   </td>
                   <td colSpan={2}>
-                    <button className="btn btn-info">Add Product</button>
+                    <button
+                      onClick={this.addNewProduct}
+                      className="btn btn-info"
+                    >
+                      Add Product
+                    </button>
                   </td>
                 </tr>
               </tfoot>
