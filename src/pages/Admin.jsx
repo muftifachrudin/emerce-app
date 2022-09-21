183280b20cdac4d1b901e0d6fcfb4a4728078c70
muftifachrudin <muftiarifachrudin@gmail.com>
2022-09-22 01:50:27 +0700

    add layout admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
 
-const Admin = () => {
+class Admin extends React.Component {
+  render() {
     return (
-        <div>
-            <h1>Admin Page</h1>
+      <div className="p-5">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>Manage Products</h1>
+            <table className="table mt-4">
+              <thead className="thead-light">
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Image</th>
+                  <th>Description</th>
+                  <th>Category</th>
+                  <th colSpan={2}>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button className="btn btn-secondary">Edit</button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+              <tfoot className="bg-light">
+                <tr>
+                  <td></td>
+                  <td>
+                    <input type="text" className="form-control" />
+                  </td>
+                  <td>
+                    <input type="text" className="form-control" />
+                  </td>
+                  <td>
+                    <input type="text" className="form-control" />
+                  </td>
+                  <td>
+                    <input type="text" className="form-control" />
+                  </td>
+                  <td>
+                    <select name="addCategory" className="form-control">
+                      <option value="">All Items</option>
+                      <option value="baju">Baju</option>
+                      <option value="topi">Topi</option>
+                      <option value="celana">Celana</option>
+                    </select>
+                  </td>
+                  <td colSpan={2}>
+                    <button className="btn btn-info">Add Product</button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
 export default Admin;
